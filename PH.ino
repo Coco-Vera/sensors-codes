66db91b1540a448501c68309a0a84427f51570d6
SaifAbdelrazek011 <saifamr1234567@gmail.com>
2025-04-23 01:04:57 +0200

    Adding pH sensor code

---
+++ PH.ino
@@ -0,0 +1,31 @@
+#define PH_PIN A0
+#define TDS_PIN A1
+#define TURBIDITY_PIN A2
+
+#define PUMP_RELAY 7
+#define VALVE1_RELAY 8
+#define VALVE2_RELAY 9
+
+#define VREF 5.0
+#define SCOUNT 30 
+
+// ==== Calibration Factors ====
+float tdsFactor = 1.256;
+float pHCalibrationFactor = -5.7;
+float pHIntercept = 17.825;
+
+// ==== Buffers ====
+int tdsBuffer[SCOUNT];
+int bufferIndex = 0;
+
+// ==== Timing ====
+unsigned long startTime;void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+float phVoltage = analogRead(PH_PIN) * (5.0 / 1024.0);
+  float pHValue = -1.57 * phVoltage + 17.825;
+}
